d6cd9a990b43cfa717115830778897893069819f
ohhaus <ohhaus84@gmail.com>
2025-11-19 23:05:37 +0300

    changed settings in engine.py

--- app/database/engine.py
+++ app/database/engine.py
@@ -8,14 +8,14 @@ def create_db_engine(connection_string: str) -> AsyncEngine:
     url = make_url(connection_string)
 
     timeout_kwargs = {
-        'pool_timeout': settings.DATABASE_ENGINE_POOL_TIMEOUT,
-        'pool_recycle': settings.DATABASE_ENGINE_POOL_RECYCLE,
-        'pool_size': settings.DATABASE_ENGINE_POOL_SIZE,
-        'max_overflow': settings.DATABASE_ENGINE_MAX_OVERFLOW,
-        'pool_pre_ping': settings.DATABASE_ENGINE_POOL_PING,
+        'pool_timeout': settings.database.POOL_TIMEOUT,
+        'pool_recycle': settings.database.POOL_RECYCLE,
+        'pool_size': settings.database.POOL_SIZE,
+        'max_overflow': settings.database.MAX_OVERFLOW,
+        'pool_pre_ping': settings.database.POOL_PING,
     }
 
     return create_async_engine(url, **timeout_kwargs)
 
 
-engine = create_db_engine(settings.SQLALCHEMY_DATABASE_URI)
+engine = create_db_engine(settings.database.URL)
